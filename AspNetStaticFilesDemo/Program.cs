7b1cc474df80060a344848fccf9ca59d209c1cbe
Gabe Shapiro <gabes@microsoft.com>
2022-03-22 15:43:05 -0700

    first commit

---
+++ AspNetStaticFilesDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+
+    // serve static files, using a path other than the application's wwwroot folder
+    app.UseStaticFiles(new StaticFileOptions()
+    {
+        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "/../../../mounts/byos1/wwwroot")),
+        RequestPath = new PathString("")
+    });
+}
+
+if (app.Environment.IsDevelopment())
+{
+    // serve static files, using the application's wwwroot folder
+	app.UseStaticFiles(new StaticFileOptions());
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
